a2e9202d6169556aa9f3586f3eef78b150f7674c
root <root@e2e-82-122.ssdcloudindia.net>
2024-01-17 21:07:04 +0530

    basic codes

---
+++ basic_dependency.sh
@@ -0,0 +1,2 @@
+pip install transformers
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
--- code_tunnel.sh
+++ code_tunnel.sh
@@ -1,3 +1,5 @@
 curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
 tar -xf vscode_cli.tar.gz
-code tunnel
+rm -rf vscode_cli.tar.gz
+mv code ..
+./../code tunnel
