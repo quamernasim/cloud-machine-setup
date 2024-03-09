a41a12e533d64925cfca04cb96f1745e48da6a0a
quamernasim <quamer23nasim38@gmail.com>
2024-03-09 09:27:10 +0000

    Add conda shell.bash hook to miniconda_install.sh

--- miniconda_install.sh
+++ miniconda_install.sh
@@ -2,3 +2,4 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 mv Miniconda3-latest-Linux-x86_64.sh ..
 bash ../Miniconda3-latest-Linux-x86_64.sh
 conda create --name quamer --clone base
+# eval "$(/home/ubuntu/quamer/miniconda3/bin/conda shell.bash hook)"
